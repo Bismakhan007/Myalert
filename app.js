e1c9f38be4363c971c894fc0291766075065307b
The Lady Boss <123287936+Bismakhan007@users.noreply.github.com>
2023-05-26 16:58:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Question No#1
+alert("Wellcome To My Website...");
+
+
+// Question No#2
+
+alert("please enter username?");
+
+// Question No#3
+ 
+  alert("Error! please enter a vild pasword.");
+
+  // Question No#4
+
+  alert("How can I help You?");
+
+  // Question No#5
+
+  alert("Give me own feedback!");
+
+  // Question No#6
+
+  alert("Thank you! For Visite My Page...");
+
+
